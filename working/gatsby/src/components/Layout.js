b1e44d709d165f738baa5c2d0ba91bd4e5361c3e
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-09-29 20:42:34 -0400

    created pages, nav and layout

---
+++ working/gatsby/src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+
+export default function Layout() { 
+  return (
+    <div>
+      <Nav />
+      <p>I am the page content</p>;
+    </div>
+  );
+}
