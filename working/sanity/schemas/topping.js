679c8da864bce11b4c5c775a75c19ac9613a692c
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-10-06 19:03:05 -0400

    creating schema

---
+++ working/sanity/schemas/topping.js
@@ -0,0 +1,33 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // cms name
+  name: 'topping',
+  // human readable title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'topping',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of the Topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is this topping vegetarian',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+  },
+};
