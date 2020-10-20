c4b4883ae34db0121b4b0ffe0b5839002c903e80
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-10-20 19:51:02 -0400

    continued

--- working/gatsby/gatsby-config.js
+++ working/gatsby/gatsby-config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
@@ -10,8 +14,11 @@ export default {
       // this is the name of the plugin we are adding
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId: ,
-      }
-    }
+        projectId: 'gos5ohby',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
   ],
 };
