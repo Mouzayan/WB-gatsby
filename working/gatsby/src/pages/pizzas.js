04cc7a0214fe6822b8391ef35f28c8dca4b6a0e1
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-10-21 20:34:32 -0400

    pulling graphql data into gatsby

--- working/gatsby/src/pages/pizzas.js
+++ working/gatsby/src/pages/pizzas.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  console.log(data);
+  const pizzas = data.pizza.nodes;
   return (
     <>
-      <p>Hey I'm the Pizzas page!</p>
+      <p>Hey! There are {pizzas.length} pizzas </p>
     </>
   );
 }
+export const query = graphql`
+  query {
+    pizza: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
